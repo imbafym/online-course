f789e04eb50e190a227741dccfbe19314c7e6292
imbafym <707082902@qq.com>
2017-11-22 14:25:47 +1100

    initial version

---
+++ OnlinecourseApplication.java
@@ -0,0 +1,12 @@
+package com.example.onlinecourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinecourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinecourseApplication.class, args);
+	}
+}
